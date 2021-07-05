304059af879078a01c6d3a107cc99ba8d7fdedd0
Kieryn Moore <kierynmoore@hotmail.com>
2021-07-05 18:33:03 +0300

    created app. finished login controller that requests spotify user to authorize app to access data

---
+++ app/controllers/api/v1/login_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::LoginController < ApplicationController
+
+  def spotify_request
+    url = "https://accounts.spotify.com/authorize"
+    query_params = {
+      client_id: ENV['CLIENT_ID'],
+      response_type: 'code',
+      redirect_uri: 'http://localhost:3000/api/v1/user',
+      scope: "user-read-private
+      user-read-recently-played
+      user-read-currently-playing
+      user-modify-playback-state",
+     show_dialog: true
+    }
+    redirect_to "#{url}?#{query_params.to_query}"
+  end
+end
