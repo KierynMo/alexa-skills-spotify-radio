181d30d9f93fe4cd9094c71ad97f3d7c7a566dca
Kieryn Moore <kierynmoore@hotmail.com>
2021-07-14 16:57:32 +0300

    changed base route to login#spotify_request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: 'api/v1/login#spotify_request'
   namespace :api do
     namespace :v1 do
       get '/login', to: "login#spotify_request"
